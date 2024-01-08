7608bc3805c9c54a6928dfc23d00f01091662a46
Bartosz <bar.klonowski@student.po.edu.pl>
2024-01-08 13:14:33 +0100

    refactor: add Random Forest

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import glob
 import itertools
 
-import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from sklearn.metrics import confusion_matrix, roc_curve, auc, precision_recall_curve
+from joblib import dump
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, roc_curve, auc, accuracy_score, precision_recall_curve
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, SimpleRNN, Dropout, LSTM
-from tensorflow.keras.models import Sequential, Sequential
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
 
 
 def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blues):
@@ -67,54 +65,22 @@ all_labels_categorical = tf.keras.utils.to_categorical(all_labels_encoded, num_c
 X_train, X_test, y_train, y_test = train_test_split(all_features_scaled, all_labels_categorical, test_size=0.2,
                                                     random_state=42)
 
-# dnn_model = Sequential([
-#     Dense(512, activation='relu', input_shape=(X_train.shape[1],), kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-#     Dropout(0.5),
-#     Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
-#     Dropout(0.5),
-#     Dense(128, activation='relu'),
-#     Dropout(0.5),
-#     Dense(num_emotions, activation='softmax')
-# ])
-#
-# dnn_model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.01, momentum=0.9), loss='categorical_crossentropy',
-#                   metrics=['accuracy'])
-#
-# early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-#
-# dnn_model.fit(X_train, y_train, epochs=50, batch_size=64, validation_split=0.2, callbacks=[early_stopping])
 dnn_model = Sequential([
-    Dense(512, activation='relu', input_shape=(X_train.shape[1],)),
+    Dense(512, activation='relu', input_shape=(X_train.shape[1],), kernel_regularizer=tf.keras.regularizers.l2(0.001)),
     Dropout(0.5),
-    Dense(256, activation='relu'),
+    Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
     Dropout(0.5),
     Dense(128, activation='relu'),
     Dropout(0.5),
     Dense(num_emotions, activation='softmax')
 ])
 
-# Inicjalna szybkość uczenia
-initial_learning_rate = 0.01
+dnn_model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.01, momentum=0.9), loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
-# Funkcja adaptacyjnej szybkości uczenia
-def lr_scheduler(epoch, lr):
-    decay_rate = 0.1
-    decay_step = 10
-    if epoch % decay_step == 0 and epoch:
-        return lr * decay_rate
-    return lr
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 
-# Kompilacja modelu z optymalizatorem Adam
-dnn_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learning_rate),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-# Dodanie LearningRateScheduler do callbacks
-callbacks = [LearningRateScheduler(lr_scheduler, verbose=1)]
-
-# Trenowanie modelu
-history = dnn_model.fit(X_train, y_train, epochs=50, batch_size=64,
-                    validation_data=(X_test, y_test), callbacks=callbacks)
+dnn_model.fit(X_train, y_train, epochs=50, batch_size=64, validation_split=0.2, callbacks=[early_stopping])
 
 # Ewaluacja modelu
 test_loss, test_accuracy = dnn_model.evaluate(X_test, y_test)
@@ -264,95 +230,52 @@ plt.ylabel('Precision')
 plt.title('Precision-Recall curve')
 plt.savefig('rnn_precision_recall_curve.png')
 
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-def load_images_in_chunks(file_paths, chunk_size, img_size=(64, 64)):
-    chunk_images = []
-    chunk_labels = []
+# Przygotowanie etykiet dla modelu Random Forest
+y_train_rf = np.argmax(y_train, axis=1)
+y_test_rf = np.argmax(y_test, axis=1)
 
-    for i, img_path in enumerate(file_paths):
-        img = cv2.imread(img_path)
-        img = cv2.resize(img, img_size)
-        img = (img / 255.0).astype('float32')
+# Trenowanie modelu na danych treningowych
+rf_model.fit(X_train, y_train_rf)
 
-        label = img_path.split('_')[-2]
-        chunk_images.append(img)
-        chunk_labels.append(label)
+# Predykcja na danych testowych
+y_pred_rf = rf_model.predict(X_test)
 
-        if (i + 1) % chunk_size == 0 or (i + 1) == len(file_paths):
-            yield np.array(chunk_images), np.array(chunk_labels)
-            chunk_images = []
-            chunk_labels = []
+# Ocena modelu
+test_accuracy_RF = accuracy_score(y_test_rf, y_pred_rf)
+print("Random Forest Test accuracy:", test_accuracy_RF)
+dump(rf_model, 'D:/ML/Diploma_work/Models/HandLandmarksModelRF')
 
+# Macierz pomyłek
+cm_rf = confusion_matrix(y_test_rf, y_pred_rf)
+plt.figure()
+plot_confusion_matrix(cm_rf, classes=label_encoder.classes_, title='Random Forest Confusion Matrix')
+plt.savefig('rf_confusion_matrix.png')
 
-img_paths = glob.glob('E:/Datasety/heatmaps/*.png')
-chunk_size = 5
-num_emotions = 6
-
-all_images = []
-all_labels = []
-
-for images, labels in load_images_in_chunks(img_paths, chunk_size):
-    all_images.extend(images)
-    all_labels.extend(labels)
-
-all_images = np.array(all_images)
-label_encoder = LabelEncoder()
-integer_encoded = label_encoder.fit_transform(all_labels)
-one_hot_encoded = to_categorical(integer_encoded, num_classes=num_emotions)
-
-X_train, X_test, y_train, y_test = train_test_split(all_images, one_hot_encoded, test_size=0.2, random_state=42)
-
-cnn_model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
-    MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dense(num_emotions, activation='softmax')
-])
-cnn_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-cnn_model.fit(X_train, y_train, batch_size=32, epochs=15, validation_data=(X_test, y_test), callbacks=[early_stopping])
-
-test_loss, test_accuracy_CNN = cnn_model.evaluate(X_test, y_test)
-print("Test CNN accuracy:", test_accuracy_CNN)
-
-cnn_model.save('D:/ML/Diploma_work/Models/HandLandmarksModelCNN')
-
-y_pred = cnn_model.predict(X_test)
-y_test_labels = np.argmax(y_test, axis=1)
-y_pred_labels = np.argmax(y_pred, axis=1)
-
-cm = confusion_matrix(y_test_labels, y_pred_labels)
-np.set_printoptions(precision=2)
+# Krzywa ROC
+fpr_rf, tpr_rf, _ = roc_curve(y_test.ravel(), rf_model.predict_proba(X_test).ravel())
+roc_auc_rf = auc(fpr_rf, tpr_rf)
 
 plt.figure()
-plot_confusion_matrix(cm, classes=label_encoder.classes_, title='Confusion matrix')
-plt.savefig('cnn_confusion_matrix.png')
-
-fpr, tpr, _ = roc_curve(y_test.ravel(), y_pred.ravel())
-roc_auc = auc(fpr, tpr)
-
-plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot(fpr_rf, tpr_rf, color='darkorange', lw=2, label='Random Forest ROC curve (area = %0.2f)' % roc_auc_rf)
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
+plt.title('Random Forest Receiver Operating Characteristic')
 plt.legend(loc="lower right")
-plt.savefig('cnn_roc_curve.png')
+plt.savefig('rf_roc_curve.png')
 
-precision, recall, _ = precision_recall_curve(y_test.ravel(), y_pred.ravel())
+precision, recall, _ = precision_recall_curve(y_test_rf.ravel(), y_pred_rf.ravel())
 
 plt.figure()
 plt.plot(recall, precision, color='blue', lw=2)
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.title('Precision-Recall curve')
-plt.savefig('cnn_precision_recall_curve.png')
+plt.savefig('RF_precision_recall_curve.png')
 
 print("Test DNN Accuracy :", test_accuracy_DNN)
-print("Test CNN Accuracy :", test_accuracy_CNN)
+print("Test RF Accuracy :", test_accuracy_RF)
 print("Test FCNN Accuracy :", test_accuracy_FCNN)
 print("Test RNN Accuracy :", test_accuracy_LSTM)
